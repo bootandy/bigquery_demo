4a83e818da71f9a546decd3a8222788430404896
andyboot <bootandy@gmail.com>
2021-06-25 08:51:35 +0000

    Update run_script
    Process the correct log file from window_functions.
    
    Some odd things happen with log file rotation. The file is renamed when
    the system is restarted but the system keeps logging to the newly moved
    file

--- run_script.sh
+++ run_script.sh
@@ -3,5 +3,7 @@
 export GOOGLE_APPLICATION_CREDENTIALS="key.json"
 
 cd /home/andy/bigquery_demo/
-/home/andy/.virtualenvs/bigquery_demo/bin/python   src/__init__.py  /var/log/window_functions.log
+file=`ls -t /var/log/old_win_func/window_functions* | head  -1`
+
+/home/andy/.virtualenvs/bigquery_demo/bin/python   src/__init__.py  $file
 
