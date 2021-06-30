aaf69d2aca8c876b20eff05a89838d9a0808aaa6
andy.boot <bootandy@gmail.com>
2021-06-30 14:28:08 +0100

    Handle log files that change names
    When service is restarted the log file changes name - that means we want
    to reset the state of the logfile so we can read all of it.
    
    Note this won't process all logs if service restarted multiple times in
    a day. This is not the 'correct' way to do things

--- run_script.sh
+++ run_script.sh
@@ -3,7 +3,7 @@
 export GOOGLE_APPLICATION_CREDENTIALS="key.json"
 
 cd /home/andy/bigquery_demo/
-file=`ls -t /var/log/old_win_func/window_functions* | head  -1`
+file=`ls -t /var/log/old_win_func/window_functions* | head  -2`
 
 /home/andy/.virtualenvs/bigquery_demo/bin/python   src/__init__.py  $file
 
